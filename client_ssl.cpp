ba21ed2efa91941cbbbe916230dc18f76eb13e35
hydra <nguyentrungduc08@gmail.com>
2018-01-17 16:21:00 +0700

    update test client

--- client_ssl.cpp
+++ client_ssl.cpp
@@ -32,8 +32,10 @@
 #include <sys/socket.h>
 #include <resolv.h>
 #include <netdb.h>
+#include <string>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/conf.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #define FAIL    -1
@@ -85,6 +87,9 @@ SSL_CTX* InitCTX(void)
     SSL_load_error_strings();			/* Bring in and register error messages */
     method = TLSv1_2_client_method();		/* Create new client-method instance */
     ctx = SSL_CTX_new(method);			/* Create new context */
+
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+
     if ( ctx == NULL )
     {
         ERR_print_errors_fp(stderr);
@@ -92,6 +97,7 @@ SSL_CTX* InitCTX(void)
     }
 
     SSL_CTX_use_certificate_file(ctx, "certificate.pem", SSL_FILETYPE_PEM);
+    //SSL_CTX_load_verify_locations(ctx, "certificate.pem", NULL);
 
     // Load trusted CAs from default paths.
     //SSL_CTX_set_default_verify_paths(ctx);
@@ -131,7 +137,9 @@ void ShowCerts(SSL* ssl)
 int main(int count, char *strings[])
 {   SSL_CTX *ctx;
     int server;
+    int res = 1;
     SSL *ssl;
+    BIO *web = NULL, *out = NULL;
     char buf[1024];
     int bytes;
     char *hostname, *portnum;
@@ -146,22 +154,94 @@ int main(int count, char *strings[])
 
     ctx = InitCTX();
    
+    web = BIO_new_ssl_connect(ctx);
+    //std::string  hostn = std::string(hostname)+":"+ std::string(portnum);
+    std::string  hostn = "localhost:4242";
+
+    printf("%s\n",hostn.c_str());
+    res = BIO_set_conn_hostname(web, hostn.c_str());
+
+    if(!(1 == res)) 
+        printf("error conn bio\n");
+
+    BIO_get_ssl(web, &ssl);
+    if(!(ssl != NULL)) 
+        printf("error cretae ssl");
+
+    const char* const PREFERRED_CIPHERS = "HIGH:!aNULL:!kRSA:!PSK:!SRP:!MD5:!RC4";
+    res = SSL_set_cipher_list(ssl, PREFERRED_CIPHERS);
+    if(!(1 == res)) 
+        printf("error cretae preferred cipher");
+
+
+    res = SSL_set_tlsext_host_name(ssl, hostname);
+    if(!(1 == res)) 
+        printf("error1\n");
+
+    out = BIO_new_fp(stdout, BIO_NOCLOSE);
+    if(!(NULL != out)) 
+        printf("error2\n");
+
+    res = BIO_do_connect(web);
+    if(!(1 == res)) 
+        printf("error3\n");
+
+    res = BIO_do_handshake(web);
+    if(!(1 == res)) 
+        printf("error4\n");
+
+
+    X509* cert = SSL_get_peer_certificate(ssl);
+    if(cert) { X509_free(cert); } /* Free immediately */
+    if(NULL == cert) 
+        printf("error5\n");
+
+    /* Step 2: verify the result of chain verification */
+    /* Verification performed according to RFC 4158    */
+    res = SSL_get_verify_result(ssl);
+    if(!(X509_V_OK == res)) 
+        printf("error6\n");
+
+    printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
+    ShowCerts(ssl); 
+    char *msg = "Hello??? send to server aaaaa";
+    BIO_write(web, msg, strlen(msg));
+
+    bytes = BIO_read(web, buf, sizeof(buf));    // get reply & decrypt 
+    buf[bytes] = 0;
+    printf("Received: \"%s\"\n", buf);
+
+
+    if(out)
+        BIO_free(out);
+
+    if(web != NULL)
+        BIO_free_all(web);
+
+    if(NULL != ctx)
+        SSL_CTX_free(ctx);
+
+    /*
+    
     server = OpenConnection(hostname, atoi(portnum));
-    ssl = SSL_new(ctx);						/* create new SSL connection state */
-    SSL_set_fd(ssl, server);				/* attach the socket descriptor */
-    if ( SSL_connect(ssl) == FAIL )			/* perform the connection */
+    ssl = SSL_new(ctx);						
+
+    SSL_set_fd(ssl, server);				// attach the socket descriptor 
+    if ( SSL_connect(ssl) == FAIL )			// perform the connection 
         ERR_print_errors_fp(stderr);
     else
     {   char *msg = "Hello??? send to server aaaaa";
 
         printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
-        ShowCerts(ssl);								/* get any certs */
-        SSL_write(ssl, msg, strlen(msg));			/* encrypt & send message */
-        bytes = SSL_read(ssl, buf, sizeof(buf));	/* get reply & decrypt */
+        ShowCerts(ssl);								// get any certs 
+        SSL_write(ssl, msg, strlen(msg));			// encrypt & send message 
+        bytes = SSL_read(ssl, buf, sizeof(buf));	// get reply & decrypt 
         buf[bytes] = 0;
         printf("Received: \"%s\"\n", buf);
-        SSL_free(ssl);								/* release connection state */
+        SSL_free(ssl);								// release connection state 
     }
+    */
+    
     //SSL_free(ssl);
     close(server);									/* close socket */
     SSL_CTX_free(ctx);								/* release context */
